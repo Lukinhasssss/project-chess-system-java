b05e8c2405204aa2c4b0c47612e25c02fbb6b412
Lucas Monteiro <lukasmonteir0@hotmail.com>
2020-12-12 09:50:21 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,34 @@
+package boardgame; // Pacote correspondente a camada de tabuleiro
+
+public class Position {
+	
+	private int row;
+	private int column;
+	
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	
+	public void setRow(int row) {
+		this.row = row;
+	}
+	
+	public int getColumn() {
+		return column;
+	}
+	
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() { // toString para imprimir a posição na tela
+		return row + ", " + column;
+	}
+
+}
