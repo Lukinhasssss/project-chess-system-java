a1ff14b6aef49fa927d44bcc2c12ca70ab5c42c9
Lucas Monteiro <lukasmonteir0@hotmail.com>
2020-12-16 10:14:44 -0300

    Queen

---
+++ src/chess/pieces/Queen.java
@@ -0,0 +1,108 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Queen extends ChessPiece { // Rook == Torre
+
+	public Queen(Board board, Color color) {
+		super(board, color);
+	}
+	
+	@Override
+	public String toString() {
+		return "Q";
+	}
+	
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] matriz = new boolean[getBoard().getRows()][getBoard().getColumns()]; // Por padrão todas as posições dessa matriz começam com false.
+		
+		Position p = new Position(0, 0);
+		
+		// above
+		p.setValues(position.getRow() - 1, position.getColumn());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { // Enquanto a posição p existir e não tiver uma peça lá, ou seja, enquanto a posição estiver vaga eu vou marcar essa posição como true...
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// left
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { // Enquanto a posição p existir e não tiver uma peça lá, ou seja, enquanto a posição estiver vaga eu vou marcar essa posição como true...
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// right
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { // Enquanto a posição p existir e não tiver uma peça lá, ou seja, enquanto a posição estiver vaga eu vou marcar essa posição como true...
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// below
+		p.setValues(position.getRow() + 1, position.getColumn());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { // Enquanto a posição p existir e não tiver uma peça lá, ou seja, enquanto a posição estiver vaga eu vou marcar essa posição como true...
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// NorthWest --> Noroeste
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() - 1, p.getColumn() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// NorthEast --> Nordeste
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() - 1, p.getColumn() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// SouthEast --> Sudeste
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// SouthWest --> Sudoeste
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		
+		return matriz;
+	}
+
+}
